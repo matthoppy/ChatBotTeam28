61d5d1724f92098fc5f9ac3e2c32f79b076cfc80
Chance <39140976+ChanceBaker@users.noreply.github.com>
2018-05-23 10:50:17 +1200

    Add files via upload

---
+++ AUTChatBot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=autchatbot-Web-Deploy.pubxml -p:Password=aKqNvvhqLw2TaifCsEeepkY9N5j0X4yLrmDtSpvJCiB2sxAm39dLqh1rToNM
+
